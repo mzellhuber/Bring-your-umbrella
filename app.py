922d7241c99f998f6e5c7af7ed8a8774ab7d2dae
Meli <melissa.zellhuber@gmail.com>
2018-04-17 23:14:51 -0500

    int condition code

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def makeWebhookResult(req):
 			print(condition_text)
 			print(condition_code)
 
-			if condition_code in [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,35,37,38,39,40,41,42,43,45,46,47]:
+			if int(condition_code) in [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,35,37,38,39,40,41,42,43,45,46,47]:
 				print("bring umbrella")
 
 				return {
