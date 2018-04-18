71f36dee130fa5732e824839b7d63473c70ee93f
Meli <melissa.zellhuber@gmail.com>
2018-04-17 22:05:33 -0500

    get from city

--- app.py
+++ app.py
@@ -50,7 +50,8 @@ def makeWebhookResult(req):
 	if req.get("result").get("action") == "get-location":
 		city = req.get("result").get("parameters").get("geo-city")
 		baseurl = "https://query.yahooapis.com/v1/public/yql?"
-		yql_query = "select wind from weather.forecast where woeid=2460286"
+		#yql_query = "select wind from weather.forecast where woeid=2460286"
+		yql_query = 'select item.condition.text from weather.forecast where woeid in (select woeid from geo.places(1) where text="'+city+'")'
 		yql_url = baseurl + urllib.urlencode({'q':yql_query}) + "&format=json"
 		result = urllib2.urlopen(yql_url).read()
 		data = json.loads(result)
