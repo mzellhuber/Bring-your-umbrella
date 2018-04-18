8937e7f97cef8a360573e8674661de13ffc9b506
Meli <melissa.zellhuber@gmail.com>
2018-04-17 21:49:54 -0500

    ()

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def makeWebhookResult(req):
 		yql_url = baseurl + urllib.urlencode({'q':yql_query}) + "&format=json"
 		result = urllib2.urlopen(yql_url).read()
 		data = json.loads(result)
-		print data['query']['results']
+		print(data['query']['results'])
 
 
 
