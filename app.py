22c8ddb61a10101ae164cae65607dc80e4040394
Meli <melissa.zellhuber@gmail.com>
2018-04-17 21:55:42 -0500

    python 2

--- app.py
+++ app.py
@@ -5,14 +5,7 @@ import pprint
 import json
 import os
 import requests
-import urllib
-
-try:
-    # For Python 3.0 and later
-    from urllib.request import urlopen
-except ImportError:
-    # Fall back to Python 2's urllib2
-    from urllib2 import urlopen
+import urllib2, urllib
 
 from flask import Flask
 from flask import request
